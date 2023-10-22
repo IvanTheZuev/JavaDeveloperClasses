dca542f227af66955f1c7070266c6ee574ef8ddd
Ivan Zuev <i9819856754@gmail.com>
2023-10-22 18:10:10 +0300

    ДЗ №10

--- src/com/schneider/project/companystructure/Department.java
+++
@@ -1,8 +0,0 @@
-package com.schneider.project.companystructure;
-
-public class Department {
-    private String name;
-    private int numberOfEmployee;
-    private Department head;
-
-}
--- src/com/schneider/project/lesson10/clonequals/Auth.java
+++
@@ -1,6 +0,0 @@
-package com.schneider.project.lesson10.clonequals;
-
-public interface Auth {
-    boolean login(String userName, String password);
-    boolean logout();
-}
